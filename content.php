24f501dbe4599382bbfbe6d840cda0ef66486635
iam-ebube <ebubeezeudu@gmail.com>
2025-07-19 19:58:30 +0100

    Add files via upload

---
+++ content.php
@@ -0,0 +1,192 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head><title>SB WINES</title></head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta content="A Sudsidary of gide NIG. ENT .Dealers and supplier of all kind of Champagne,Assorted Fruits Juices,Spring Water,Irish Creams etc. With over 40 years of experiences, we offer the best of prenium whiskeys,champagnes,spring waters,asssorted fruit juices,soft drinks.
+	We are focused on giving you the best of drinks..with us you can drink with confidence.In God we trust.Our HEAD OFFICE at #15 Mission Road, Benin City while our BRANCH OFFICE at shop 8 Ede Shopping Plaza Vegetable Market, Opp.The NIG. Observer Airport Road Benin City." name="description">
+<link rel="stylesheet" href="w3css/W3CSS.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="../../cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+<link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+ <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  
+ <!-- Favicons -->
+  <link href="drinks/sb.png" rel="icon">
+  <link href="drinks/Logo.png" rel="apple-touch-icon">  
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-none d-lg-block">
+    <div class="container d-flex">
+      <div class="contact-info mr-auto">
+        <ul>
+          <li><i class="icofont-envelope"></i><a href="mailto:contact@example.com">contact :ebubeezeudu@gmail.com</a></li>
+          <li><i class="icofont-phone"></i><a href="call:09076342705"> +234 816 975 9025</a></li>
+          <li><i class="icofont-clock-time icofont-flip-horizontal"></i> Mon-sat 7am - 6pm</li>
+        </ul>
+
+      </div>
+     
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="container d-flex">
+
+      <div class="logo mr-auto">
+        <h1 class="text-light"><a href="index.html"><span>SBWINES</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href=" sb.html #about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#team">Team</a></li>
+          <li><a href="#pricing">Pricing</a></li>
+          <li class="active"><a href="content.php">Wines</a></li>
+          <li class="drop-down"><a href="">More</a>
+            <ul>
+              <li><a href="#media">Media</a></li>
+              
+                          
+            </ul>
+          </li>
+          <li><a href="#contact">Contact</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+  
+  <div style="margin-left:6.5%" class="w3-opacity">
+  <?php
+  
+  if(isset($_POST['myform'])){
+  echo "Dear";
+  echo" ";  
+  echo  $email = $_POST['email'];
+  echo"<br />";  
+  echo "Welcome to Sbwine Shop what are you getting from us today?";
+  echo"<br />";
+  
+  
+  
+  }
+  
+  
+  
+  ?>
+  <br />
+  </div>
+
+<div id="wines">
+
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Glenfiddich <i  class="w3-tag w3-red w3-margin-left">$100</i></div>
+        <img src="drinks/glenfiddich-aged-18-years-.jpg" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">4th Street <i  class="w3-tag w3-red w3-margin-left">$5</i></div>
+        <img src="drinks/4TH-STREET-SWEET-RED-WINE-750ML.jpg" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Bacardi <i  class="w3-tag w3-red w3-margin-left">$12</i></div>
+        <img src="drinks/1.jpg" alt="wines" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Jameson Black Barrels <i  class="w3-tag w3-red w3-margin-left">$22</i></div>
+        <img src="drinks/51yuu-ruzRL._AC_SY879_.jpg" alt="wines" style="width:100%">
+      </div>
+    </div>
+  </div>
+
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Jameson Green <i  class="w3-tag w3-red w3-margin-left">$7</i></div>
+        <img src="drinks/jameson_fortis_sku_70cl_gr_p_web.jpg" alt="wines" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Chamdor Wine <i  class="w3-tag w3-red w3-margin-left">$3</i></div>
+        <img src="drinks/Chamdor-Red-Sparkling-Grape-750ml.jpg" alt="wines" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Eva Wine <i  class="w3-tag w3-red w3-margin-left">$3</i></div>
+        <img src="drinks/eva-wine.jpg" alt="wines" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Remymartins Vsop <i  class="w3-tag w3-red w3-margin-left">$53</i> </div>
+        <img src="drinks/REMY-MARTIN-VSOP.jpg" alt="wines" style="width:99%">
+      </div>
+    </div>
+  </div>
+  
+  
+  
+  </div>
+   </body>
+</html> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
